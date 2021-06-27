967e5b4d74e27d678c87f170c13dd59f77baeadf
Generosa Litton <glcodeworks@gmail.com>
2021-06-26 19:14:23 -0500

    function for loop review

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-  document.querySelectorAll('.btn').forEach(item => {
-    item.addEventListener('click', ()=> {
-      alert('this is awesome')
-    });
-  });
+const catNames = ["Django", "Luna", "Booboo", "gusto", "clea", "sofi"];
+const lastName = ["Litton"];
+
+let newArray = [];
+
+for (let i = 0; i < catNames.length; i++) {
+  const fullName = `${catNames[i]} ${lastName}`;
+  newArray.push(fullName);
+  // console.log(fullName);
+}
+
+// Expenses Function
+
+const gas = [30, 45, 40, 65];
+const food = [80, 75, 100];
+
+function calculateTotal(arr) {
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
+
+const gasTotal = calculateTotal(gas);
+const foodTotal = calculateTotal(food);
+
+console.log({
+  gas: gasTotal,
+  food: foodTotal,
+});
